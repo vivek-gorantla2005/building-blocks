9cc07030a43d6bac0627c4d059994f966a0212fa
vivekgorantla <23eg105j66@anurag.edu.in>
2025-10-16 04:16:08 +0530

    ai-providers added

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { helloWorld } from "../../../inngest/functions";
+import { execute } from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld,
+    execute
   ],
 });
